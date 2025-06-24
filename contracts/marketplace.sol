9b0d0893acef69c63376d395fc89509cb0c1602f
Sarthak <sarthakbansalog@gmail.com>
2025-06-24 11:35:55 +0530

    Added Marketplace contract

---
+++ contracts/marketplace.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./RWAAsset.sol";
+import "./Admin.sol";
+import "./IssuerRegistry.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard {
+    
+}
