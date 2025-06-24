ebe53c5cf208d6e3b538a1c4ab8df9d3aac06ae6
Sarthak <sarthakbansalog@gmail.com>
2025-06-24 14:09:29 +0530

    Updated Header Component

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -16,8 +16,8 @@ const Header = () => {
       icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />
     },
     {
-      name: "Contact",
-      link: "#contact",
+      name: "Tokenize",
+      link: "/issuer",
       icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
     }
   ];
