b6d9b457e54dfb95dfcffb239d47b1dc2b728042
Sarthak <sarthakbansalog@gmail.com>
2025-06-24 11:34:11 +0530

    Added functions to issuer contract

--- contracts/IssuerRegistry.sol
+++ contracts/IssuerRegistry.sol
@@ -14,5 +14,49 @@ contract IssuerRegistry {
         bool hasIssuerCap;
     }
 
+    mapping(address => IssuerInfo) public issuers;
+    address[] public issuerList;
+
+    event IssuerAdded(address indexed issuer, string name);
+    event IssuerDeactivated(address indexed issuer);
+    event IssuerCapGranted(address indexed issuer);
+
+    modifier onlyAdmin() {
+        require(msg.sender == adminContract.getAdmin(), "Not admin");
+        _;
+    }
+
+    constructor(address _adminContract) {
+        require(_adminContract != address(0), "Invalid admin contract");
+        adminContract = Admin(_adminContract);
+    }
+
+    function addIssuer(
+        address issuer,
+        string memory name,
+        string memory metadataUri
+    ) external onlyAdmin {
+        require(!issuers[issuer].active, "Issuer already exists");
+        
+        issuers[issuer] = IssuerInfo({
+            issuer: issuer,
+            name: name,
+            metadataUri: metadataUri,
+            active: true,
+            hasIssuerCap: false
+        });
+        
+        issuerList.push(issuer);
+        emit IssuerAdded(issuer, name);
+    }
+
+    function grantIssuerCap(address issuer) external onlyAdmin {
+        require(issuers[issuer].active, "Issuer not active");
+        require(!issuers[issuer].hasIssuerCap, "Already has issuer cap");
+        
+        issuers[issuer].hasIssuerCap = true;
+        emit IssuerCapGranted(issuer);
+    }
+
     
 }
